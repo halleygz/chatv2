69b60d2e3ba40cc1632b56d16cf47c5a32838af6
halleygz <halleluya2013idone@gmail.com>
2024-06-16 19:07:01 +0300

    modal project done

--- DOM-practice/prac2/script.js
+++ DOM-practice/prac2/script.js
@@ -1,1 +1,17 @@
-console.log("working");
+//variables
+const openBtn = document.getElementById('open-btn')
+const modalContainer = document.getElementById('modal-container')
+const closeBtn = document.getElementById('close-btn')
+
+//event listners
+openBtn.addEventListener('click', function(){
+    modalContainer.style.display = 'block'
+})
+closeBtn.addEventListener('click', function(){
+    modalContainer.style.display = 'none'
+})
+window.addEventListener('click', function(e){
+    if(e.target === modalContainer){
+        modalContainer.style.display = 'none'
+    }
+})
