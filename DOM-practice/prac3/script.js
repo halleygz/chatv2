13cc63f9fa9c9be2eecf5d0a6e4c820c5b2e3d29
halleygz <halleluya2013idone@gmail.com>
2024-06-16 19:32:33 +0300

    accordion practice completed

--- DOM-practice/prac3/script.js
+++ DOM-practice/prac3/script.js
@@ -1,1 +1,8 @@
-console.log("working");
+//variales
+const accordion = document.getElementsByClassName("content-container");
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
