28c9ee19e61d8ffd6a389085ef623530b353d114
halleygz <halleluya2013idone@gmail.com>
2024-06-23 15:55:58 +0300

    changes on data.js

--- DOM-practice/react/airbnb/data.js
+++ DOM-practice/react/airbnb/data.js
@@ -41,4 +41,18 @@ export default [
     location: "Norway",
     openSpots: 3,
   },
+  {
+    id: 3,
+    title: "Group Mountain Biking",
+    description:
+      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+    price: 50,
+    coverImg: "mountain-bike.png",
+    stats: {
+      rating: 4.8,
+      reviewCount: 2,
+    },
+    location: "Norway",
+    openSpots: 3,
+  },
 ];
