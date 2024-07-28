955119761266c1861a9b744f0b867a6e35ac8e01
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-07-28 21:06:32 +0300

    initialized app

--- NodeJS/web-servers/src/app.js
+++ NodeJS/web-servers/src/app.js
@@ -2,8 +2,20 @@ import express from 'express'
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hellow exp')
+
+app.use(express.static('public'))
+
+
+app.get('/about', (req, res)=>{
+    res.send('about page')
+})
+
+app.get('/weather', (req, res)=>{
+    res.send('show weather page')
+})
+
+app.get('/help', (req, res)=>{
+    res.send('help here')
 })
 
 app.listen(3000, ()=> {
