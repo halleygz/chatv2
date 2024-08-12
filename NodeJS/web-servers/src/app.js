48419a1667e299529cd95074dae718ed633d5c16
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-12 12:42:08 +0300

    added express package and intialized express

--- NodeJS/web-servers/src/app.js
+++ NodeJS/web-servers/src/app.js
@@ -5,9 +5,11 @@ const app = express()
 
 app.use(express.static('public'))
 
+app.set('view engine', 'ejs')
+
 
 app.get('/about', (req, res)=>{
-    res.send('about page')
+    res.render('index')
 })
 
 app.get('/weather', (req, res)=>{
